3369d6a303d63c1d3c0302e8ae62911b191f2ff1
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-09-02 22:30:30 +0200

    added cors

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getFlashcards } from "./model.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
 const port = 3601;
 app.get("/", (req, res) => {
     res.send(` 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { getFlashcards } from "./model.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3601;
 
 app.get("/", (req: express.Request, res: express.Response) => {
